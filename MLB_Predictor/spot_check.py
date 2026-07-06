d8f777de8b042c97d8fb9ac0369508a313490a96
jchavez951-ctrl <jchavez951@gmail.com>
2026-07-06 09:30:04 -0700

    Update spot_check.py

--- MLB_Predictor/spot_check.py
+++ MLB_Predictor/spot_check.py
@@ -15,3 +15,10 @@ try:
 except FileNotFoundError:
     st.error(f"Could not find rosters.json at: {file_path}")
     st.stop()
+    # Change the path logic to look in the parent directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# The '..' tells Python to look one folder up
+file_path = os.path.join(current_dir, '..', 'rosters.json') 
+
+with open(file_path, 'r') as f:
+    ROSTER_DATABASE = json.load(f)
